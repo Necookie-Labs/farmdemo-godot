7ebda70e49159b26e76949563703656c01946afa
Necookie <dheyn.main@gmail.com>
2026-03-16 22:15:38 +0800

    added walking and idle animation

--- Nodes/player.gd
+++ Nodes/player.gd
@@ -1,4 +1,3 @@
-
 extends CharacterBody2D
 
 @export var max_speed: float = 80.0
@@ -6,6 +5,8 @@ extends CharacterBody2D
 @export var friction: float = 800.0
 
 var last_input_dir: Vector2 = Vector2.DOWN
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
@@ -20,3 +21,26 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
 	move_and_slide()
+	update_animation(input_dir)
+
+func update_animation(input_dir: Vector2) -> void:
+	if input_dir != Vector2.ZERO:
+		if abs(input_dir.x) > abs(input_dir.y):
+			animated_sprite.play("walk_side")
+			animated_sprite.flip_h = input_dir.x < 0
+		elif input_dir.y < 0:
+			animated_sprite.play("walk_up")
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.play("walk_down")
+			animated_sprite.flip_h = false
+	else:
+		if abs(last_input_dir.x) > abs(last_input_dir.y):
+			animated_sprite.play("idle_side")
+			animated_sprite.flip_h = last_input_dir.x < 0
+		elif last_input_dir.y < 0:
+			animated_sprite.play("idle_up")
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.play("idle_down")
+			animated_sprite.flip_h = false
